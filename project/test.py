051759c1a681296a0d0d3e153ca8dcd5735a9d81
samyareh <test@test.com>
2024-12-23 14:49:18 +0300

    Revert "i"
    This reverts commit 4db0c5f3c979ef484d49361c8a5be083b858eef4.

--- project/test.py
+++ project/test.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 print("this a test file")
 print("Many men ")
-=======
-print("max(1, 2, 3) = ", max(1, 2, 3))
-print("myname is maximus")
-print("this a test file")
-print("Hello World")
->>>>>>> parent of 11a77fd (yy)
